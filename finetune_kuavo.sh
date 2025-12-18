b45c39f17cd1550feb1af629f79d3f9a4a5ecbc2
dazazh <berryibrahimmdy@gmail.com>
2025-12-18 21:53:12 +0800

    update initial version of finetuning

--- finetune_kuavo.sh
+++ finetune_kuavo.sh
@@ -1,15 +1,19 @@
 # set -x -e
-export WANDB_MODE=offline
-export NUM_GPUS=4
-export RANK=0 WORLD_SIZE=1 MASTER_ADDR=localhost MASTER_PORT=29500
+export WANDB_API_KEY=7c5f305e84248e078c1e77d32d2592d31d2bc834
+export WANDB_PROJECT=GR00T_leju
+export NUM_GPUS=2
+export CUDA_VISIBLE_DEVICES=0,1
+export RANK=0 WORLD_SIZE=2 MASTER_ADDR=localhost MASTER_PORT=29500
+# 设置 Hugging Face 缓存目录到大容量存储路径
+export HF_HOME="/mnt/pfs/scalelab2/Humanoid/hf_cache"
+# 增加 NCCL 超时时间到 1 小时 (默认 10 分钟)，防止因数据加载慢导致训练中断
+export NCCL_TIMEOUT=3600
+export TORCH_NCCL_BLOCKING_WAIT=1
 
-# 1. 禁用 P2P，解决 4090 通信不稳定问题
-export NCCL_P2P_DISABLE=1
-export NCCL_IB_DISABLE=1
 
 uv run torchrun --nproc_per_node=$NUM_GPUS --master_port=$MASTER_PORT \
     gr00t/experiment/launch_finetune.py \
-    --base_model_path nvidia/GR00T-N1.6-3B \
+    --base_model_path ckpt/kuavo_finetune/checkpoint-4000 \
     --dataset_path  demo_data/single_goods_orders \
     --embodiment_tag LEJU_KUAVO \
     --num_gpus $NUM_GPUS \
